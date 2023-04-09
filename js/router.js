a26ce545671c8e99e869aa5d5888197b98d66031
Poofs <77813904+PoofyPloop@users.noreply.github.com>
2023-04-08 21:54:45 -0400

    Spa router and new pages

--- js/router.js
+++ js/router.js
@@ -1,7 +1,26 @@
 const route = (event) => {
     event = event || window.event;
-    event.preventDefault();
+    event.preventDefault()
     window.history.pushState({}, "", event.target.href)
+    handlerRoute()
 }
 
-window.route = route;
+const routes = {
+    404: "/pages/404.html",
+    "/": "/pages/home.html",
+    "/about": "/pages/about.html",
+    "/gitfolio": "/dist/index.html",
+    "/contact": "/pages/contacts.html",
+}
+
+const handlerRoute = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404]
+    const html = await fetch(route).then((data) => data.text())
+    document.getElementById("main-page").innerHTML = html
+}
+
+window.onpopstate = handlerRoute
+window.route = route
+
+handlerRoute()
